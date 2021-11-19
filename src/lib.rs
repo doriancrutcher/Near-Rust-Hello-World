d90d860c3e0e71ea41898e062532d55e6956d2b0
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2021-11-18 16:39:43 -0800

    implemented lookupmaps

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,37 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
+use near_sdk::env;
 use near_sdk::near_bindgen;
 
 near_sdk::setup_alloc!();
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct HelloWorld {
-    name: String,
+    hello_message: LookupMap<String, String>,
+}
+
+impl Default for HelloWorld {
+    fn default() -> Self {
+        Self {
+            hello_message: LookupMap::new(b"a".to_vec()),
+        }
+    }
 }
 
 #[near_bindgen]
 impl HelloWorld {
     pub fn set_name(&mut self, name: String) {
-        self.name = format!("{} {}{}", "Hello", name, "!");
+        self.hello_message
+            .insert(&name, &format!("{} {}{}", "Hello", name, "!"));
+        let print_message = format!("Hello {}", name);
+        env::log(print_message.as_bytes())
     }
 
-    pub fn get_message(&self) -> String {
-        let result = self.name.clone();
-        result
+    pub fn get_message(&self, name: String) -> String {
+        match self.hello_message.get(&name) {
+            Some(message) => message,
+            None => "No message found".to_string(),
+        }
     }
 }
